90c38883381254f8f1567023864a40d8d532605d
Robel Tessema <robeltessema.h@gmail.com>
2020-06-14 08:17:02 -0500

    Disable the consul discovery client
    For this to work the consul dependencies in the framework is also removed and a new mvn clean install is necessary

--- src/main/java/edu/miu/cs544/group4/engine/Application.java
+++ src/main/java/edu/miu/cs544/group4/engine/Application.java
@@ -2,14 +2,13 @@ package edu.miu.cs544.group4.engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication(
 		scanBasePackages = "edu.miu.common, edu.miu.common.config, edu.miu.cs.cs544, edu.miu.cs544.group4.engine")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
