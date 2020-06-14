f5ebc1d04da1554afbb5ccccef31337fafb0339c
nshehata <nshehata@jiosdev.com>
2020-06-14 17:19:54 -0600

    Adding the AdminController

---
+++ src/main/java/edu/miu/cs544/group4/engine/controller/AdminController.java
@@ -0,0 +1,31 @@
+package edu.miu.cs544.group4.engine.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/")
+public class AdminController {
+
+	// Test method will be removed later
+	@GetMapping("hello")
+	public String sayHello() {
+		return "Hello World";
+	}
+
+	// Properties
+	
+	
+	
+	
+	
+	// Airline APIs
+	
+	
+	
+	
+	
+	// Airport APIs
+
+}
