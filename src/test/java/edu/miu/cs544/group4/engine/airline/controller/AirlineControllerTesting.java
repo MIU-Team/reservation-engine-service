e313111eb5a79c591d30d44179a1107fa0f12668
Dawed Nesru <dawednsr@gmail.com>
2020-06-17 12:16:58 -0500

    Revert "this is the latest from me"
    This reverts commit 3bbced59c58a31b1b1a2014cfc7c166c1020975d.

--- src/test/java/edu/miu/cs544/group4/engine/airline/controller/AirlineControllerTesting.java
+++ src/test/java/edu/miu/cs544/group4/engine/airline/controller/AirlineControllerTesting.java
@@ -13,7 +13,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import edu.miu.common.exception.ResourceNotFoundException;
 import edu.miu.cs544.group4.engine.controller.AdminController;
 import edu.miu.cs544.group4.engine.model.Address;
 import edu.miu.cs544.group4.engine.model.Airline;
@@ -50,14 +49,14 @@ public class AirlineControllerTesting {
 
     
     @Test
-    void testSaveAirline() throws ResourceNotFoundException{
+    void testSaveAirline() {
         when(airlineService.saveAirline(airlines.get(1))).thenReturn(airlines.get(1));
         assertEquals(airlines.get(1), adminController.saveAirline(airlines.get(1)));
         verify(airlineService).saveAirline(airlines.get(1));
     }   
     
     @Test
-    void testGetAirline() throws ResourceNotFoundException{
+    void testGetAirline() {
     when(airlineService.getAirlineById(airlines.get(1).getId())).thenReturn(airlines.get(1));
         assertEquals(airlines.get(1), adminController.getAirline(airlines.get(1).getId()));
         verify(airlineService).getAirlineById(airlines.get(1).getId());
