5329089ab509a7955beebbb9203421850aff26c8
Robel Tessema <robeltessema.h@gmail.com>
2020-06-18 07:46:03 -0500

    Enable swagger authorization from the engine side

--- src/main/java/edu/miu/cs544/group4/engine/configuration/SwaggerConfig.java
+++ src/main/java/edu/miu/cs544/group4/engine/configuration/SwaggerConfig.java
@@ -1,24 +1,29 @@
 package edu.miu.cs544.group4.engine.configuration;
 
+import static io.swagger.models.auth.In.HEADER;
+import static java.util.Collections.singletonList;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -28,13 +33,23 @@ public class SwaggerConfig {
 	public Docket api() throws IOException, XmlPullParserException {
 		MavenXpp3Reader reader = new MavenXpp3Reader();
 		Model model = reader.read(new FileReader("pom.xml"));
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("edu.miu.cs544.group4.engine"))
-				.paths(PathSelectors.any())
-				.build()
-				.securitySchemes(new ArrayList<>(Arrays.asList(new ApiKey("Bearer", "Authorization", "Header"))))
-				.apiInfo((apiInfo()));
+		
+		return new Docket(SWAGGER_2)
+		        .securitySchemes(singletonList(new ApiKey("JWT", AUTHORIZATION, HEADER.name())))
+		        .securityContexts(singletonList(
+		            SecurityContext.builder()
+		                .securityReferences(
+		                    singletonList(SecurityReference.builder()
+		                        .reference("JWT")
+		                        .scopes(new AuthorizationScope[0])
+		                        .build()
+		                    )
+		                )
+		                .build())
+		        ).apiInfo((apiInfo()))
+		        .select()
+		        .apis(RequestHandlerSelectors.basePackage("edu.miu.cs544.group4.engine"))
+		        .build();
 	}
 
 	private ApiInfo apiInfo() {
