99ae82487a1ad991e114bd7fb02efad2008869dd
knguyen93 <khanhnt.tdt@gmail.com>
2020-06-17 23:42:54 -0500

    remove mandatory message for email

--- src/main/java/edu/miu/cs544/group4/engine/service/request/ConfirmReservationRequest.java
+++ src/main/java/edu/miu/cs544/group4/engine/service/request/ConfirmReservationRequest.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Data
 @ToString
 public class ConfirmReservationRequest {
-    @NotBlank(message = "Email is mandatory")
     private String email;
     @NotBlank(message = "Reservation code is mandatory")
     private String reservationCode;
